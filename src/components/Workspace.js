8b023406598942aa0508274d27c057cd8600fba4
Roshan Kenia <62453990+roshankenia@users.noreply.github.com>
2021-09-23 22:49:28 -0400

    Item Loading v1

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -1,19 +1,44 @@
 import React from "react";
 
 export default class Workspace extends React.Component {
-    render() {
-        return (
-            <div id="top5-workspace">
-                <div id="workspace-edit">
-                    <div id="edit-numbering">
-                        <div className="item-number">1.</div>
-                        <div className="item-number">2.</div>
-                        <div className="item-number">3.</div>
-                        <div className="item-number">4.</div>
-                        <div className="item-number">5.</div>
-                    </div>
-                </div>
+  render() {
+    const { currentList } = this.props;
+
+    if (currentList) {
+      return (
+        <div id="top5-workspace">
+          <div id="workspace-edit">
+            <div id="edit-numbering">
+              <div className="item-number">1.</div>
+              <div className="item-number">2.</div>
+              <div className="item-number">3.</div>
+              <div className="item-number">4.</div>
+              <div className="item-number">5.</div>
             </div>
-        )
+            <div id = "edit-items">
+                <div id="item-1" class="top5-item" draggable="true">{currentList.items[0]}</div>
+                <div id="item-2" class="top5-item" draggable="true">{currentList.items[1]}</div>
+                <div id="item-3" class="top5-item" draggable="true">{currentList.items[2]}</div>
+                <div id="item-4" class="top5-item" draggable="true">{currentList.items[3]}</div>
+                <div id="item-5" class="top5-item" draggable="true">{currentList.items[4]}</div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div id="top5-workspace">
+          <div id="workspace-edit">
+            <div id="edit-numbering">
+              <div className="item-number">1.</div>
+              <div className="item-number">2.</div>
+              <div className="item-number">3.</div>
+              <div className="item-number">4.</div>
+              <div className="item-number">5.</div>
+            </div>
+          </div>
+        </div>
+      );
     }
+  }
 }
